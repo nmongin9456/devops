165b84eb7b96a03fe4fa37095e8270a522002cd0
Nicolas MONGIN <nmongin@gmail.com>
2016-12-20 14:54:16 +0100

    suppression vmwarephp

--- vmwarephp/library/Vmwarephp/Exception/InvalidVhost.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Vmwarephp\Exception;
-
-class InvalidVhost extends \Exception {
-	function __construct($message = null, $code = 0, Exception $previous = null) {
-		$defaultMessage = 'Invalid vCenter/ESX host provided';
-		parent::__construct($message ? : $defaultMessage, $code, $previous);
-	}
-}
